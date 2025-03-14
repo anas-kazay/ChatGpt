0f346bce8ae408dcea1030a3cc739989f4ae9b7e
anasKazay <anaskazay@gmail.com>
2025-03-14 01:34:00 +0000

    modify the app

--- src/main/java/anas/kazay/chatapp/repository/ModelRepository.java
+++
@@ -1,9 +0,0 @@
-package anas.kazay.chatapp.repository;
-
-import anas.kazay.chatapp.model.Model;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ModelRepository extends JpaRepository<Model, Long> {
-    Optional<Model> findByName(String name);
-}
--- src/main/java/anas/kazay/chatapp/service/AIService.java
+++
@@ -1,7 +0,0 @@
-package anas.kazay.chatapp.service;
-
-import anas.kazay.chatapp.model.User;
-
-public interface AIService {
-    String generateResponse(String userQuestion, User user, Long modelId);
-}
